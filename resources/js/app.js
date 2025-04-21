fa589c8cebff6c13329a802a2a456bdad2abdb83
Rafal <rafspy11@gmail.com>
2025-04-21 21:17:24 +0200

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+// import './bootstrap';
+import '../libs/fontawesome-free-6.7.2-web/css/all.min.css';
+import '../libs/fontawesome-free-6.7.2-web/js/all.min.js';
+import '../libs/mdb5/css/mdb.min.css';
+import '../libs/mdb5/js/mdb.es.min.js';
+import '../libs/mdb5/js/mdb.umd.min.js';
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+    <x-slot:title>
+        Strona główna
+    </x-slot>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h1>Hello World!</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-layout>
