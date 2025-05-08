91fd7ad5ff3c19e5d4e1a4cc50f22002516e94e6
Rafal <web@zabart.com>
2025-05-08 08:55:02 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,17 @@
     </x-slot>
     <section>
         <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h1>Hello World!</h1>
+            <div class="row justify-content-center">
+                <div class="col-auto">
+                    <div class="input-group">
+                        <div class="form-outline" data-mdb-input-init>
+                            <input type="search" id="form1" class="form-control" />
+                            <label class="form-label" for="form1">Szukaj</label>
+                        </div>
+                        <button type="button" class="btn btn-primary" data-mdb-ripple-init>
+                            <i class="fas fa-search"></i>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
